e525bddecb14642d508091bba63a60244d8c8744
John Paulo Geronimo <fx99@ph.sunlife>
2023-01-16 23:20:05 +0800

    Fix Navigation Bar

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
